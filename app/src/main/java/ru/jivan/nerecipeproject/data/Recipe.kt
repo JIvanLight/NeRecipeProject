90d995023465bc76b96ef91da1e444831dfa6ec3
Ivan Zharkov <qwer_steel@mail.ru>
2022-09-14 05:24:15 +0300

    Development stage. UI and models (3). Adding Room Library support.

--- app/src/main/java/ru/jivan/nerecipeproject/data/Recipe.kt
+++ app/src/main/java/ru/jivan/nerecipeproject/data/Recipe.kt
@@ -1,5 +1,8 @@
 package ru.jivan.nerecipeproject.data
 
+import ru.jivan.nerecipeproject.R
+import ru.jivan.nerecipeproject.utill.App
+
 data class Recipe(
     val id: Long,
     val title: String,
@@ -7,4 +10,37 @@ data class Recipe(
     val category: String,
     val position: Long,
     val favorite: Boolean = false
-)
+) {
+    companion object {
+        enum class RecipeCategory(val categoryName: String, val localizedName: String) {
+            European(
+                "European",
+                App.getAppResources().getString(R.string.european)
+            ),
+            Asian(
+                "Asian",
+                App.getAppResources().getString(R.string.asian)
+            ),
+            PanAsian(
+                "PanAsian",
+                App.getAppResources().getString(R.string.pan_asian)
+            ),
+            Eastern(
+                "Eastern",
+                App.getAppResources().getString(R.string.eastern)
+            ),
+            American(
+                "American",
+                App.getAppResources().getString(R.string.american)
+            ),
+            Russian(
+                "Russian",
+                App.getAppResources().getString(R.string.russian)
+            ),
+            Mediterranean(
+                "Mediterranean",
+                App.getAppResources().getString(R.string.mediterranean)
+            )
+        }
+    }
+}
